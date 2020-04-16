ca38967d1e0da6558c84876c9a5b3c1dcaaa9c12
Shekhar <shekhar59324@gmail.com>
2020-04-16 21:07:37 +0530

    Added Fragments and containers

--- app/src/main/java/com/solvabit/loadersandfragments/MainActivity.kt
+++ app/src/main/java/com/solvabit/loadersandfragments/MainActivity.kt
@@ -2,11 +2,24 @@ package com.solvabit.loadersandfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.solvabit.loadersandfragments.fragments.LoaderFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var importLoader: LoaderFragment
+    lateinit var importLoader2: LoaderFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, importLoader )
+                .addToBackStack(importLoader.toString())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+        }
     }
 }
