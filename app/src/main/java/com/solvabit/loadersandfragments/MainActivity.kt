ac1d3d20bf7820259aeb28bb3432e2fe1383cfed
Shekhar <shekhar59324@gmail.com>
2020-04-16 22:07:21 +0530

    Added a Loader No.1

--- app/src/main/java/com/solvabit/loadersandfragments/MainActivity.kt
+++ app/src/main/java/com/solvabit/loadersandfragments/MainActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import com.solvabit.loadersandfragments.fragments.LoaderFragment
+import com.solvabit.loadersandfragments.fragments.LoaderFragment2
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     lateinit var importLoader: LoaderFragment
-    lateinit var importLoader2: LoaderFragment
+    lateinit var importLoader2: LoaderFragment2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        importLoader = LoaderFragment.newInstance()
         button.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, importLoader )
--- app/src/main/java/com/solvabit/loadersandfragments/fragments/LoaderFragment.kt
+++ app/src/main/java/com/solvabit/loadersandfragments/fragments/LoaderFragment.kt
@@ -19,6 +19,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoaderFragment : Fragment() {
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,17 +42,9 @@ class LoaderFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoaderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             LoaderFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
